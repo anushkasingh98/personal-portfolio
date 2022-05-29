d36196fb4d0e64a3fea3311844a18f94c28d1e02
Anushka Singh <anushka@anushkasingh.com>
2022-05-30 02:18:15 +0530

    Add blog urls

--- personal_portfolio/urls.py
+++ personal_portfolio/urls.py
@@ -1,4 +1,4 @@
-"""personal_portfolio URL Configuration
+"""personal_portfolio Blog URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
